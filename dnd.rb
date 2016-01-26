481848b13707c00cef88cfbdfec82fd693562034
Tom Reznick <thomas.reznick@gmail.com>
2016-01-25 19:23:48 -0500

    use ability mod formula (instead of lookup case statement

--- dnd.rb
+++ dnd.rb
@@ -63,41 +63,9 @@ module DnD
 
 
   def self.ability_score_for(stat)
-    case stat
-    when 1
-      -5
-    when 2..3
-      -4
-    when 4..5
-      -3
-    when 6..7
-      -2
-    when 8..9
-      -1
-    when 10..11
-      0
-    when 12..13
-      1
-    when 14..15
-      2
-    when 16..17
-      3
-    when 18..19
-      4
-    when 20..21
-      5
-    when 22..23
-      6
-    when 24..25
-      7
-    when 26..27
-      8
-    when 28..29
-      9
-    when 30
-      10
-    else
-      30
+    if stat < 1 || stat > 30
+      raise ArgumentError, "must be within 1-30"
     end
+    ( ( stat - 10.0) / 2 ).floor
   end
 end
